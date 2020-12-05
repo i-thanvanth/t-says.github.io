b7c3906503a9575bf330d2863313867931b81781
i-thanvanth <40958787+i-thanvanth@users.noreply.github.com>
2020-12-05 09:17:38 +0530

    fixed the hb -menu not popping up

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ const navbarLinks = document.getElementsByClassName('navbar-elements')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
-
-function myFunction() {
-  var x = document.getElementById("button__content");
-
-    if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-} 
